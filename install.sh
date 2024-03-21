0b4bb4fcf352311b4b97067320bb077e119ac6d5
fanj1216yxing <36510359+fanj1216yxing@users.noreply.github.com>
2024-03-21 16:28:47 +0800

    Update and rename README.md to install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+bash <(wget -qO- https://raw.githubusercontent.com/jerry048/Dedicated-Seedbox/main/Install.sh) -u aaa -p 123456 -c 3072 -q 4.3.9 -l v1.2.19 -3
+wget https://git.io/vDstU -O transmission-install.sh
+sh transmission-install.sh
+y
+admin
+adminadmin
+adminadmin
+wget https://github.com/ronggang/transmission-web-control/raw/master/release/install-tr-control-cn.sh
+bash install-tr-control-cn.sh
+1
+1
+sudo fallocate -l 16G /swapfile
+ls -lh /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo swapon --show
+free -h
+apt install lsof
