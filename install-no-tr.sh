75bbb580a7d589bc0ebf751d7e98de9ff16e1406
fanj1216yxing <36510359+fanj1216yxing@users.noreply.github.com>
2024-03-25 12:14:10 +0800

    Update install-no-tr.sh

--- install-no-tr.sh
+++ install-no-tr.sh
@@ -1,9 +1,9 @@
 bash <(wget -qO- https://raw.githubusercontent.com/jerry048/Dedicated-Seedbox/main/Install.sh) -u aaa -p 123456 -c 3072 -q 4.3.9 -l v1.2.19 -3
 sudo fallocate -l 16G /swapfile
 ls -lh /swapfile
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
-sudo swapon --show
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+swapon --show
 free -h
 bash <(wget -qO- https://raw.githubusercontent.com/fanj1216yxing/sb-quickinstall/main/better.sh)
