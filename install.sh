e767d8882f77fd6b7fc0f057ff2a0df4f00e6667
fanj1216yxing <36510359+fanj1216yxing@users.noreply.github.com>
2024-03-25 11:46:54 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 bash <(wget -qO- https://raw.githubusercontent.com/jerry048/Dedicated-Seedbox/main/Install.sh) -u aaa -p 123456 -c 3072 -q 4.3.9 -l v1.2.19 -3
-bash <(wget -qO- https://raw.githubusercontent.com/jerry048/Dedicated-Seedbox/main/tr.sh)
+bash <(wget -qO- https://raw.githubusercontent.com/fanj1216yxing/sb-quickinstall/main/tr.sh)
 1
 1
 fallocate -l 16G /swapfile
