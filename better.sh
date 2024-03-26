2115617cb45c8c46fb82d5307b23add2116ee51e
fanj1216yxing <36510359+fanj1216yxing@users.noreply.github.com>
2024-03-26 21:49:23 +0800

    Update better.sh

--- better.sh
+++ better.sh
@@ -1,3 +1,7 @@
 apt install lsof
 echo '* - nofile 999900' | sudo tee -a /etc/security/limits.conf
 ulimit -n 999900
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+sysctl -p
+lsmod | grep bbr
