a964ae1b489fbbc32bd15817e414813254e4b75c
fanj1216yxing <36510359+fanj1216yxing@users.noreply.github.com>
2024-03-26 21:36:20 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+apt-get update&&apt-get upgrade
+apt-get -y purge apache2-* bind9-* xinetd samba-* nscd-* portmap sendmail-* sasl2-bin
+apt-get -y purge lynx memtester unixodbc python-* odbcinst-* sudo tcpdump ttf-*
+apt-get autoremove && apt-get clean
 bash <(wget -qO- https://raw.githubusercontent.com/jerry048/Dedicated-Seedbox/main/Install.sh) -u aaa -p 123456 -c 3072 -q 4.3.9 -l v1.2.19 -3
 bash <(wget -qO- https://raw.githubusercontent.com/fanj1216yxing/sb-quickinstall/main/tr.sh)
 1
