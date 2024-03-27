663a5c038760c68230438da5dac5c718e7b82ef0
fanj1216yxing <36510359+fanj1216yxing@users.noreply.github.com>
2024-03-27 12:06:00 +0800

    Update nps.sh

--- nps.sh
+++ nps.sh
@@ -1,6 +1,6 @@
 wget --no-check-certificate https://lnmp.club/lnmp1.9-full.tar.gz
 md5sum lnmp1.9-full.tar.gz
-tar zxf lnmp1.9-full.tar.gz && cd lnmp1.9-full && LNMP_Auto="y" DBSelect="10" Bin="n" DB_Root_Password="99905230523" InstallInnodb="y" PHPSelect="12" SelectMalloc="1" ./install.sh lnmp
+tar zxf lnmp1.9-full.tar.gz && cd lnmp1.9-full && LNMP_Auto="y" DBSelect="10" Bin="n" DB_Root_Password="password230523" InstallInnodb="y" PHPSelect="12" SelectMalloc="1" ./install.sh lnmp
 cd /root/lnmp1.9-full
 ./addons.sh install opcache
 ./addons.sh install fileinfo
@@ -14,7 +14,7 @@ make && make install
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 mysql -u root -p
-99905230523
+password230523
 create database 
 nexusphp
 default charset=utf8mb4 collate utf8mb4_general_ci;
@@ -22,20 +22,20 @@ quit
 cd /home/wwwroot
 wget https://github.com/xiaomlove/nexusphp/archive/refs/tags/v1.8.10.zip
 unzip v1.8.10.zip
-mkdir ptss 
-cp -r nexusphp-1.8.8/. /home/wwwroot/ptss
-cd /home/wwwroot/ptss
+mkdir pt.demoo.moe 
+cp -r nexusphp-1.8.8/. /home/wwwroot/pt.demoo.moe
+cd /home/wwwroot/pt.demoo.moe
 composer install
-cp -R /home/wwwroot/ptss/nexus/Install/install /home/wwwroot/ptss/public/
-chmod -R 0777 /home/wwwroot/ptss
-wget --no-check-certificate https://web-dl.cc/share/ptss.conf
-mv ptss.conf /usr/local/nginx/conf/vhost/demo.nexusphp.org.conf
+cp -R /home/wwwroot/pt.demoo.moe/nexus/Install/install /home/wwwroot/pt.demoo.moe/public/
+chmod -R 0777 /home/wwwroot/pt.demoo.moe
+wget --no-check-certificate https://web-dl.cc/share/pt.demoo.moe.conf
+mv pt.demoo.moe.conf /usr/local/nginx/conf/vhost/demo.nexusphp.org.conf
 cat > /usr/local/nginx/conf/vhost/demo.nexusphp.org.conf <<- EOM
 server {
 # 以实际为准
 root RUN_PATH; 
 
-server_name ptss;
+server_name pt.demoo.moe;
 
 location / {
     index index.html index.php;
@@ -54,14 +54,11 @@ location ~ \.php {
     include fastcgi_params;
 }
 
-access_log /var/log/nginx/ptss.access.log;
-error_log /var/log/nginx/ptss.error.log;
+access_log /var/log/nginx/pt.demoo.moe.access.log;
+error_log /var/log/nginx/pt.demoo.moe.error.log;
 }
 EOM
 cd /usr/local/php/etc/php-fpm.d
 wget --no-check-certificate https://web-dl.cc/share/php-fpm-pt.conf
 echo 'include = etc/php-fpm.d/*.conf' | sudo tee -a /usr/local/php/etc/php-fpm.conf
 /etc/init.d/php-fpm restart
-chattr -i /home/wwwroot/ptss/.user.ini
-rm -f /home/wwwroot/ptss/.user.ini
-
