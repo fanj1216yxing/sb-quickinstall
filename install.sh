e6944ce0b11627a3cb345ffd2d034222a03ae3fa
fanj1216yxing <36510359+fanj1216yxing@users.noreply.github.com>
2024-04-01 18:58:43 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 apt-get update
 apt install curl unzip wget -y
-bash <(wget -qO- https://raw.githubusercontent.com/jerry048/Dedicated-Seedbox/main/Install.sh) -u aaa -p 123456 -c 3072 -q 4.3.9 -l v1.2.19 -3
+bash <(wget -qO- https://raw.githubusercontent.com/jerry048/Dedicated-Seedbox/main/Install.sh) -u aaa -p 123456 -c 3072 -q 4.3.9 -l v1.2.19 -3 -v
 bash <(wget -qO- https://raw.githubusercontent.com/fanj1216yxing/sb-quickinstall/main/tr.sh)
 1
 1
-fallocate -l 16G /swapfile
+fallocate -l 4G /swapfile
 ls -lh /swapfile
 chmod 600 /swapfile
 mkswap /swapfile
