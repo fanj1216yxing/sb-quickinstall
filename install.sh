402c2be81b4af9cc35275a84a73522b10bc4f90d
fanj1216yxing <36510359+fanj1216yxing@users.noreply.github.com>
2024-04-02 17:51:23 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,10 +5,17 @@ bash <(wget -qO- https://raw.githubusercontent.com/fanj1216yxing/sb-quickinstall
 1
 1
 fallocate -l 4G /swapfile
+
 ls -lh /swapfile
+
 chmod 600 /swapfile
+
 mkswap /swapfile
+
 swapon /swapfile
+
 swapon --show
+
 free -h
+
 bash <(wget -qO- https://raw.githubusercontent.com/fanj1216yxing/sb-quickinstall/main/better.sh)
