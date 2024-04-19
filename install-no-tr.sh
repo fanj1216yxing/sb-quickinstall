a774902877c6777e4982bbd2fa326789ca885a3e
fanj1216yxing <36510359+fanj1216yxing@users.noreply.github.com>
2024-04-19 16:17:59 +0800

    Update install-no-tr.sh

--- install-no-tr.sh
+++ install-no-tr.sh
@@ -1,6 +1,6 @@
 apt-get update
 apt install curl unzip wget -y
-bash <(wget -qO- https://raw.githubusercontent.com/jerry048/Dedicated-Seedbox/main/Install.sh) -u aaa -p 123456 -q 4.3.9 -l v1.2.19
+bash <(wget -qO- https://raw.githubusercontent.com/jerry048/Dedicated-Seedbox/main/Install.sh) -u aaa -p 123456  -c 1500 -q 4.3.9 -l v1.2.19
 sudo fallocate -l 1G /swapfile
 ls -lh /swapfile
 chmod 600 /swapfile
