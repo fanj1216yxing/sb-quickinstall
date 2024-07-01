443142c18ce6f5b38f15e9cfff55ba0e83783570
fanj1216yxing <36510359+fanj1216yxing@users.noreply.github.com>
2024-07-01 22:58:21 +0800

    Create bj.sh

---
+++ bj.sh
@@ -0,0 +1,15 @@
+echo "[program:joal]" >> /etc/supervisor/conf.d/joal.conf
+echo "priority=1" >> /etc/supervisor/conf.d/joal.conf
+echo "command=sleep 20 && /usr/bin/java -jar jack-of-all-trades-2.1.36.jar --joal-conf="/opt/joal" --spring.main.web-environment=true --server.port=3105 --joal.ui.path.prefix="imlala" --joal.ui.secret-token="lala.im"
+autostart=true" >> /etc/supervisor/conf.d/joal.conf
+echo "autostart=true" >> /etc/supervisor/conf.d/joal.conf
+echo "autorestart=true" >> /etc/supervisor/conf.d/joal.conf
+echo "redirect_stderr=true" >> /etc/supervisor/conf.d/joal.conf
+echo "stdout_logfile=/var/log/supervisor/joal.log" >> /etc/supervisor/conf.d/joal.conf
+sudo chmod -R 777 /opt
+supervisorctl update
+supervisorctl status joal
+sleep 30
+wget -P /opt/joal https://raw.githubusercontent.com/fanj1216yxing/sb-quickinstall/main/config.json -O config.json
+sleep 30
+supervisorctl restart joal
