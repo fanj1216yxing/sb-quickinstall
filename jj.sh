f540b83c5195860e8842679b972159a897d205ed
fanj1216yxing <36510359+fanj1216yxing@users.noreply.github.com>
2024-07-01 20:47:12 +0800

    Update jj.sh

--- jj.sh
+++ jj.sh
@@ -5,7 +5,7 @@ apt install wget
 y
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
-mkdir -p /home/joals&& cd /home/joals
+mkdir -p /home/joals && cd /home/joals
 docker run -d -p 3105:3105 -v /home/joals:/data --name="joal" anthonyraymond/joal:2.1.36 --joal-conf="/data" --spring.main.web-environment=true --server.port="3105" --joal.ui.path.prefix="imlala" --joal.ui.secret-token="lala.im" --restart unless-stopped
-wget -P /home/joals https://raw.githubusercontent.com/fanj1216yxing/sb-quickinstall/main/config.jspn
+wget -P /home/joals https://raw.githubusercontent.com/fanj1216yxing/sb-quickinstall/main/config.json
 docker restart joal
