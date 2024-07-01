5655fa8cbb2c697187a8c6c2c4289c95eb0e6485
fanj1216yxing <36510359+fanj1216yxing@users.noreply.github.com>
2024-07-01 22:49:14 +0800

    Update jaj.sh

--- jaj.sh
+++ jaj.sh
@@ -4,7 +4,10 @@ mkdir -p /opt/joal&& cd /opt/joal
 sudo chmod -R 777 /opt
 wget https://github.com/anthonyraymond/joal/releases/download/2.1.36/joal.tar.gz
 tar -xzvf joal.tar.gz
-
+/usr/bin/java -jar jack-of-all-trades-2.1.36.jar --joal-conf="/opt/joal" --spring.main.web-environment=true --server.port=3105 --joal.ui.path.prefix="imlala" --joal.ui.secret-token="lala.im"
+autostart=true
+sleep 120
+exit
 echo "[program:joal]" >> /etc/supervisor/conf.d/joal.conf
 echo "priority=1" >> /etc/supervisor/conf.d/joal.conf
 echo "command=sleep 20 && /usr/bin/java -jar jack-of-all-trades-2.1.36.jar --joal-conf="/opt/joal" --spring.main.web-environment=true --server.port=3105 --joal.ui.path.prefix="imlala" --joal.ui.secret-token="lala.im"
