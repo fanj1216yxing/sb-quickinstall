5ad7b2d0c441d8a62b92efef8dd79d09a9ac2b9d
fanj1216yxing <36510359+fanj1216yxing@users.noreply.github.com>
2024-07-03 21:59:50 +0800

    Update swap4g.sh

--- swap4g.sh
+++ swap4g.sh
@@ -13,3 +13,11 @@ swapon --show
 free -h
 
 wapon
+apt install lsof
+echo '* - nofile 999900' | sudo tee -a /etc/security/limits.conf
+ulimit -n 999900
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+sysctl -p
+lsmod | grep bbr
+
