bcb65dc544e3ce10395946077c0812611598fd07
fanj1216yxing <36510359+fanj1216yxing@users.noreply.github.com>
2024-07-12 21:03:10 +0800

    Update bag.sh

--- beg.sh
+++ beg.sh
@@ -15,17 +15,29 @@ swapon --show
 free -h
 
 wapon
+
 apt install lsof
+
 echo '* - nofile 999900' | sudo tee -a /etc/security/limits.conf
+
 ulimit -n 999900
+
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+
 sysctl -p
+
 sysctl net.ipv4.tcp_available_congestion_control
+
 lsmod | grep bbr
+
 rmmod virtio_balloon
+
 curl -L https://raw.githubusercontent.com/spiritLHLS/one-click-installation-script/main/install_scripts/dlm.sh -o dlm.sh && chmod +x dlm.sh && bash dlm.sh
+
 curl -L https://raw.githubusercontent.com/spiritLHLS/addzram/main/addzram.sh -o addzram.sh && chmod +x addzram.sh && bash addzram.sh
+
 1
 
 10000
