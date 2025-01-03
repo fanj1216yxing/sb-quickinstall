270dc89bb6e8750ed2f506e5a9464ba4e5607727
fanj1216yxing <36510359+fanj1216yxing@users.noreply.github.com>
2025-01-03 13:14:57 +0800

    Update install-no-tr.sh

--- install-no-tr.sh
+++ install-no-tr.sh
@@ -1,5 +1,7 @@
 apt install curl unzip wget -y
-bash <(wget -qO- https://raw.githubusercontent.com/jerry048/Dedicated-Seedbox/main/Install.sh) -u aaa -p 123456  -c 1500 -q 4.3.9 -l v1.2.19
+RAM=$(free -m | awk '/^Mem:/{print $2}')
+CACHE_SIZE=$((RAM / 8))
+bash <(wget -qO- https://raw.githubusercontent.com/jerry048/Dedicated-Seedbox/main/Install.sh) -u aaa -p 123456  -c $CACHE_SIZE -q 4.3.9 -l v1.2.19
 sudo fallocate -l 1G /swapfile
 ls -lh /swapfile
 chmod 600 /swapfile
